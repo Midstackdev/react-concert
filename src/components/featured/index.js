3cf915799208e6b677d2ecc9031d89aa0e8df190
Midstackdev <midstackdev@gmail.com>
2020-07-23 17:36:24 +0000

    Countdown timer

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Carousel from './Carousel'
+import CountDown from './CountDown'
 
 const Featured = () => {
   return (
     <div style={{position: 'relative'}}>
       
       <Carousel />
-      
+
       <div className="artist_name">
         <div className="wrapper">
           Ariana Grande
         </div>
       </div>
+
+      <CountDown />
     </div>
   )
 }
