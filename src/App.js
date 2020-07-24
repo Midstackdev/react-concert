c8107010e015bb93d243597fed8e46a10beb27fc
Midstackdev <midstackdev@gmail.com>
2020-07-24 01:18:11 +0000

    Scroll to menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Element } from 'react-scroll'
+
 import './resources/styles.css'
 
 import Header from './components/layouts/Header'
@@ -15,11 +17,25 @@ class App extends Component {
     return (
       <div className="App" style={{height:"1500px", background:"cornflowerblue"}}>
         <Header/>
-        <Featured />
-        <VenueInfo />
-        <Highlights />
-        <Pricing />
-        <Location />
+
+        <Element name="featured">
+          <Featured />
+        </Element>
+
+        <Element name="venue">
+          <VenueInfo />
+        </Element>
+
+        <Element name="highlights">
+          <Highlights />
+        </Element>
+
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
         <Footer />
       </div>
     );
